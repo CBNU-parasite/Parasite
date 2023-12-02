261cd679748ed5cefd7d1f3039fbe975ae734564
JaeChanHWANG <hss598876@gmail.com>
2023-12-02 23:41:53 +0900

    Update Analizing Page
    나눠놓은 분석 페이지 플롯짜놨습니다.

--- src/main/resources/static/js/account/myPage.js
+++ src/main/resources/static/js/account/myPage.js
@@ -21,20 +21,21 @@ new Chart(caloryGraph, {
   }
 );
 
-new Chart(pieChart,{
-    type: 'pie',
-    weight:11,
-    hoverBorderJoinStyle : 'round',
-    data:{
-        labels:['탄수화물','단백질','지방'],
-        datasets:[{data: [70,10,20],
-            backgroundColor: [
-                'rgb(255, 99, 132)',
-                'rgb(54, 162, 235)',
-                'rgb(255, 205, 86)'
-            ],
-            hoverOffset: 4
+new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
     }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
     }
   }
-)
+});

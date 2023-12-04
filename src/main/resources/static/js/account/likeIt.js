04fcafba8382d6075f380115cc71254e311462d4
JaeChanHWANG <hss598876@gmail.com>
2023-12-04 23:08:00 +0900

    Fix some css files

--- src/main/resources/static/js/account/likeIt.js
+++ src/main/resources/static/js/account/likeIt.js
@@ -1,7 +1,21 @@
 function iLikeIt(button) {
+    var state = 0;
+    var btnId = document.getElementsById(button);
+
     if (button.textContent == '♡') {
         button.textContent = '♥';
+        state = 1;
     } else {
         button.textContent = '♡';
     }
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "/main", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    
+    var data = JSON.stringify({
+        btnState : state,
+        btnId : btnId
+    });
+    xhr.send(data);
 }

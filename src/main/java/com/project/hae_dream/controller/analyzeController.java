559386eee851f9633d94a894601e80354a218dea
ujin999 <epahg789@gmail.com>
2023-12-06 17:23:48 +0900

    분석하기 페이지에서 선택 누르면 선택한 데이터 받아오기

--- src/main/java/com/project/hae_dream/controller/analyzeController.java
+++ src/main/java/com/project/hae_dream/controller/analyzeController.java
@@ -1,15 +1,14 @@
 package com.project.hae_dream.controller;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.project.hae_dream.dto.FoodRequest;
 import com.project.hae_dream.util.FoodApi;
 import com.project.hae_dream.util.FoodInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -42,6 +41,28 @@ public class analyzeController {
         model.addAttribute("foodInfo", foodInfo);
 
         return "/analyze/analyzeSearch";
+    }
+
+    @PostMapping("/analyze/selectFood")
+    @ResponseBody
+    public String handleButtonClick(@ModelAttribute FoodRequest foodRequest) {
+        // 클라이언트로부터 받은 데이터를 처리합니다.
+        String foodName = foodRequest.getFoodName();
+        String nutrCont1 = foodRequest.getNutrCont1();
+        String nutrCont2 = foodRequest.getNutrCont2();
+        String nutrCont3 = foodRequest.getNutrCont3();
+        String nutrCont4 = foodRequest.getNutrCont4();
+
+        // 실제 로직을 수행하고 응답을 생성합니다.
+        String responseData = "Data received from the client: " + foodName;
+
+        System.out.println("responseData = " + responseData);
+        System.out.println("nutrCont4 = " + nutrCont4);
+        System.out.println("nutrCont3 = " + nutrCont3);
+        System.out.println("nutrCont2 = " + nutrCont2);
+        System.out.println("nutrCont1 = " + nutrCont1);
+
+        return responseData;
     }
 
     @GetMapping("/analyze/analyzeGraph")
---
+++ src/main/java/com/project/hae_dream/dto/FoodRequest.java
@@ -0,0 +1,16 @@
+package com.project.hae_dream.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FoodRequest {
+
+    private String foodName;
+    private String nutrCont1;
+    private String nutrCont2;
+    private String nutrCont3;
+    private String nutrCont4;
+
+}

2229c5f599094a729a3565ef3182b0b1c105df0a
JaeChanHWANG <hss598876@gmail.com>
2023-12-17 19:41:38 +0900

    Add function
    좋아요 눌르면 0인 숫자가 1로바뀌게 했습니다

--- src/main/resources/static/js/account/likeIt.js
+++ src/main/resources/static/js/account/likeIt.js
@@ -1,7 +1,15 @@
-function iLikeIt(button) {
+function iLikeIt(button, likeId) {
+    var elementName = "likeCnt" + likeId;
+    var likeCnt = document.getElementById(elementName);
+    var currentLikeCnt = parseInt(likeCnt.textContent);
+
     if (button.textContent == '♡') {
+        currentLikeCnt += 1;
         button.textContent = '♥';
     } else {
+        currentLikeCnt -= 1;
         button.textContent = '♡';
     }
+
+    likeCnt.textContent = currentLikeCnt;
 }

988da7872dee69ab482fc5e7d525f1fec84d92b1
JaeChanHWANG <hss598876@gmail.com>
2023-12-18 01:28:18 +0900

    Update MainPage
    샘플이미지 변경했습니다.
    보드 상위4개 읽어서 메인페이지에 게시하도록 했습니다.

--- src/main/java/com/project/hae_dream/controller/HomeController.java
+++ src/main/java/com/project/hae_dream/controller/HomeController.java
@@ -1,5 +1,9 @@
 package com.project.hae_dream.controller;
 
+import com.project.hae_dream.dto.BoardDTO;
+import com.project.hae_dream.entity.BoardEntity;
+import com.project.hae_dream.entity.BoardFileEntity;
+import com.project.hae_dream.service.BoardService;
 import com.project.hae_dream.service.MainService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -8,16 +12,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
 public class HomeController {
 
     private final MainService mainService;
+    private final BoardService boardService;
 
     @GetMapping("/index")
     public String index(Model model, HttpServletRequest request) throws NullPointerException{
+
+        List<BoardDTO> boardDTOList = boardService.findAll();
+        Collections.reverse(boardDTOList);
+
+        model.addAttribute("boardList",boardDTOList.subList(0,4));
 
         HttpSession session = request.getSession(false);
 
